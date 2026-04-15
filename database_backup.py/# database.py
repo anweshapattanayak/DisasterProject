e731a7deb110f575f4d4741d04c5d0f4f73e49ff
Anwesha Pattanayak <anwesha27pattanayak@gmail.com>
2026-04-15 21:37:24 +0530

    Removed backup file

--- database_backup.py/# database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:anwesha2627@localhost/disaster_db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
